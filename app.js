f4981d9af9b4953c888fa6cdc274d8e042d9a3ac
BennyZHill <ben.h@builtbypixel.com>
2021-07-14 14:24:52 +0100

    commit app.js base dice animation and image change

---
+++ app.js
@@ -0,0 +1,28 @@
+let images = ["diceW1.svg",
+"diceW2.svg",
+"diceW3.svg",
+"diceW4.svg",
+"diceW5.svg",
+"diceW6.svg"];
+let dice = document.querySelectorAll("img");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("rollDice");
+    });
+    setTimeout (function(){
+        dice.forEach(function(die){
+            die.classList.remove("rollDice");
+        });
+        let dieOneValue = Math.floor(Math.random()*6)
+        ;
+        document.querySelector("#die-1").setAttribute
+        ("src",images[dieOneValue]);
+        document.querySelector("#total").innerHTML = 
+        "Your total is " + (dieOneValue + 1)
+        
+
+    },
+    1000
+    );
+}
